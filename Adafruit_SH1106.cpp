b06267a2c84cf754a48083eae0b8251e6792ca00
gbob <gba-gbob@vfemail.net>
2018-04-13 21:24:12 +0100

    fixed no image initial start up

--- Adafruit_SH1106.cpp
+++ Adafruit_SH1106.cpp
@@ -207,11 +207,7 @@ void Adafruit_SH1106::begin(uint8_t vccstate, uint8_t i2caddr, bool reset) {
   {
     // I2C Init
     Wire.begin();
-#ifdef __SAM3X8E__
-    // Force 400 KHz I2C, rawr! (Uses pins 20, 21 for SDA, SCL)
-    TWI1->TWI_CWGR = 0;
-    TWI1->TWI_CWGR = ((VARIANT_MCK / (2 * 400000)) - 4) * 0x101;
-#endif
+    delay(100); //required to properly power on
   }
 
   if (reset) {
